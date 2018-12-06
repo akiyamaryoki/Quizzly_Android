52ab3794c19521a2284439c707da56179c3b21c3
Josh Kimmel <joshkimmel16@g.ucla.edu>
2018-12-05 16:06:11 -0800

    Getting the app to working POC.

--- Quizzly/app/src/main/java/com/example/joshkimmel/quizzly/MainActivity.java
+++ Quizzly/app/src/main/java/com/example/joshkimmel/quizzly/MainActivity.java
@@ -23,6 +23,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,10 +36,13 @@ import java.util.UUID;
 
 import android.os.*;
 
+import com.google.gson.JsonIOException;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONStringer;
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -54,20 +58,47 @@ public class MainActivity extends AppCompatActivity {
     private byte[] buffer;
 
     private Handler mHandler;
+    private int STATE_DISCONNECTED = 0;
+    private final int STATE_CONNECTED = 1;
+    private final int STATE_QUESTION_RECEIVED = 2;
+    private final int  STATE_SCORE_RECEIVED = 3;
+    private final int STATE_WAITING = 4;
+    private final int STATE_GAMEOVER = 5;
+    private int currentState;
 
+    // for question screen
     private TextView question;
     private RadioButton optionA;
     private RadioButton optionB;
     private RadioButton optionC;
     private RadioButton optionD;
-    private String title;
     private Button submit;
     private JSONObject currentQuestion;
+
+    // for score screen
+    private TextView place;
+    private TableLayout scoreTable;
+    private TextView playerName1;
+    private TextView playerName2;
+    private TextView playerName3;
+    private TextView playerName4;
+    private TextView playerScore1;
+    private TextView playerScore2;
+    private TextView playerScore3;
+    private TextView playerScore4;
+    private int numPlayers;
+
+    // for welcome screen
+    private TextView playerCreate;
+    private Button playerJoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentState = 0;
+        numPlayers = 0;
 
         question = (TextView) findViewById(R.id.question1);
         optionA = (RadioButton) findViewById(R.id.buttonA);
@@ -75,6 +106,20 @@ public class MainActivity extends AppCompatActivity {
         optionC = (RadioButton) findViewById(R.id.buttonC);
         optionD = (RadioButton) findViewById(R.id.buttonD);
         submit = (Button) findViewById(R.id.buttonS);
+
+        place = (TextView) findViewById(R.id.place);
+        scoreTable = (TableLayout) findViewById(R.id.scoreTable);
+        playerName1 = (TextView) findViewById(R.id.playerName1);
+        playerName2 = (TextView) findViewById(R.id.playerName2);
+        playerName3 = (TextView) findViewById(R.id.playerName3);
+        playerName4 = (TextView) findViewById(R.id.playerName4);
+        playerScore1 = (TextView) findViewById(R.id.playerScore1);
+        playerScore2 = (TextView) findViewById(R.id.playerScore2);
+        playerScore3 = (TextView) findViewById(R.id.playerScore3);
+        playerScore4 = (TextView) findViewById(R.id.playerScore4);
+
+        playerCreate = (TextView) findViewById(R.id.username);
+        playerJoin = (Button) findViewById(R.id.submit_user);
 
         bluetoothDeviceAddress = getString(R.string.bluetoothDeviceAddress);
         TAG = getString(R.string.bluetoothLogTag);
@@ -117,6 +162,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void updateQuestion(JSONObject newQuestion) {
+        optionA.setChecked(false);
+        optionB.setChecked(false);
+        optionC.setChecked(false);
+        optionD.setChecked(false);
+
         try
         {
             Log.i("Testing", newQuestion.toString());
@@ -135,15 +185,46 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.e("MYAPP", "unexpected JSON exception", e);
         }
-
+        UpdateView(STATE_QUESTION_RECEIVED, false);
     }
 
     protected void updateScore (JSONObject newScore) {
         Log.i("Testing", newScore.toString());
-        optionA.setChecked(false);
-        optionB.setChecked(false);
-        optionC.setChecked(false);
-        optionD.setChecked(false);
+
+        // Set score screen attributes
+        try {
+            JSONArray rankings = newScore.getJSONArray("rankings");
+            numPlayers = rankings.length();
+            for (int i=0; i<rankings.length(); i++) {
+                JSONObject r = rankings.getJSONObject(i);
+                switch (i) {
+                    case 0:
+                        playerName1.setText(r.get("playerName").toString());
+                        playerScore1.setText(Integer.toString(r.getInt("score")));
+                        break;
+                    case 1:
+                        playerName2.setText(r.get("playerName").toString());
+                        playerScore2.setText(Integer.toString(r.getInt("score")));
+                        break;
+                    case 2:
+                        playerName3.setText(r.get("playerName").toString());
+                        playerScore3.setText(Integer.toString(r.getInt("score")));
+                        break;
+                    case 3:
+                        playerName4.setText(r.get("playerName").toString());
+                        playerScore4.setText(Integer.toString(r.getInt("score")));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        catch (JSONException e) {
+            Log.e("MYAPP", "JSON error", e);
+        }
+
+
+        UpdateView(STATE_SCORE_RECEIVED, false);
     }
 
     // Method to handle incoming data read from the server
@@ -153,8 +234,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void OnConnect () {
-        String test = "{\"type\": \"playerCreation\", \"data\": {\"playerName\": \"Test\"}}";
-        WriteToServer(test.getBytes());
+        UpdateView(STATE_CONNECTED, false);
+        //String test = "{\"type\": \"playerCreation\", \"data\": {\"playerName\": \"Test\"}}";
+        //WriteToServer(test.getBytes());
     }
 
     // Method to enable Bluetooth on the given device
@@ -314,6 +396,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void playerCreateClick(View view) {
+        String pName = playerCreate.getText().toString();
+        JSONObject send = new JSONObject();
+        JSONObject data = new JSONObject();
+        try {
+            send.put("type", "playerCreation");
+            data.put("playerName", pName);
+            send.put("data", data);
+        }
+        catch (JSONException e) {
+            Log.e("MYAPP", "JSON error", e);
+        }
+        WriteToServer(send.toString().getBytes());
+    }
+
     public void clickHandler(View view) {
         int currentNum = -1;
         try {
@@ -360,5 +457,98 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Testing", sendBack.toString());
         WriteToServer(sendBack.toString().getBytes());
 
+    }
+
+    // Top-level method to execute a view transition
+    private void UpdateView (int state, boolean gameOver) {
+        switch (state) {
+            case STATE_CONNECTED:
+                ShowHideWelcomeScreen(false);
+                ShowHideQuestionScreen(false);
+                ShowHideScoreScreen(false);
+                ShowHidePlayerCreation(true);
+                break;
+            case STATE_WAITING:
+                ShowHidePlayerCreation(false);
+                ShowHideQuestionScreen(false);
+                ShowHideWaitingScreen(true);
+                break;
+            case STATE_QUESTION_RECEIVED:
+                ShowHidePlayerCreation(false);
+                ShowHideWelcomeScreen(false);
+                ShowHideScoreScreen(false);
+                ShowHideQuestionScreen(true);
+                break;
+            case STATE_SCORE_RECEIVED:
+                ShowHidePlayerCreation(false);
+                ShowHideWelcomeScreen(false);
+                ShowHideQuestionScreen(false);
+                ShowHideScoreScreen(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ShowHideWelcomeScreen (boolean show) {
+
+    }
+
+    private void ShowHidePlayerCreation (boolean show) {
+        int v = (show) ? View.VISIBLE : View.INVISIBLE;
+        playerCreate.setVisibility(v);
+        playerJoin.setVisibility(v);
+    }
+
+    private void ShowHideWaitingScreen (boolean show) {
+
+    }
+
+    private void ShowHideQuestionScreen (boolean show) {
+        int v = (show) ? View.VISIBLE : View.INVISIBLE;
+        question.setVisibility(v);
+        optionA.setVisibility(v);
+        optionB.setVisibility(v);
+        optionC.setVisibility(v);
+        optionD.setVisibility(v);
+        submit.setVisibility(v);
+    }
+
+    private void ShowHideScoreScreen (boolean show) {
+        int v = (show) ? View.VISIBLE : View.INVISIBLE;
+        Log.i("Testing", "Visibility is: " + Integer.toString(v));
+        //place.setVisibility(v);
+        scoreTable.setVisibility(v);
+        //for (int i = 0; i < numPlayers; i++) {
+            //switch (i) {
+                //case 0:
+                    //Log.i("Testing", "Show/hide Score " + Integer.toString(numPlayers));
+                    //playerName1.setVisibility(v);
+                    //playerScore1.setVisibility(v);
+                    //break;
+                //case 1:
+                    //playerName2.setVisibility(v);
+                    //playerScore2.setVisibility(v);
+                    //break;
+                //case 2:
+                    //playerName3.setVisibility(v);
+                    //playerScore3.setVisibility(v);
+                    //break;
+                //case 3:
+                    //playerName4.setVisibility(v);
+                    //playerScore4.setVisibility(v);
+                    //break;
+                //default:
+                    //break;
+            //}
+        //}
+        playerName1.setVisibility(v);
+        playerScore1.setVisibility(v);
+        playerName2.setVisibility(v);
+        playerScore2.setVisibility(v);
+        playerName3.setVisibility(v);
+        playerScore3.setVisibility(v);
+        playerName4.setVisibility(v);
+        playerScore4.setVisibility(v);
     }
 }
